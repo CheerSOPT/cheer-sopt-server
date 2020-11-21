23390fae32459612ee5ab6ad9aec8a326448d5a3
Changi <limchangi@naver.com>
2020-11-22 06:54:22 +0900

    fix again again

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -98,8 +98,8 @@ exports.blendRecipe = async (req, res) => {
     const drink_2_stack = drinks_idx_2.toString().repeat(ratio_three[1]).split('');
     const drink_3_stack = drinks_idx_3.toString().repeat(ratio_three[2]).split('');
     const three_recipe_stack = drink_1_stack.concat(drink_2_stack, drink_3_stack);
-    const recipeLevel_3 = confirmLevel(level_sum_3);
     const level_sum_3 = drink_1_level * ratio_three[0] + drink_2_level * ratio_three[1] + drink_3_level * ratio_three[2];
+    const recipeLevel_3 = confirmLevel(level_sum_3);
     const blended_count_3 = 3;
     const ratios = [{ drinksIdx: drinks_idx_1, ratioPercent: ratio_three[0] }, { drinksIdx: drinks_idx_2, ratioPercent: ratio_three[1] }, { drinksIdx: drinks_idx_3, ratioPercent: ratio_three[2] }];
     return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.BLEND_DRNIK_SUCCESS, { ratios, recipeLevel_3, three_recipe_stack, blended_count_3 }));
